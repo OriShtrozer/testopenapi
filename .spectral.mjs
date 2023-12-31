010b452a30259f48a1dcad44ddcec428b86199d5
orishtrozer@clarityo.com <orishtrozer@clarityo.com>
2023-12-31 16:32:50 +0200

    Added 2 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjIzMDc5Mg/spectral.js?branch_slug=master&token=8c3d98c5-60a5-41a6-a2ec-7d1db0710b3d";
+export default { extends: ruleset };
